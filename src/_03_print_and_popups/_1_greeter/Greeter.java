cd60c7e4570ae4060eeb2443cfade87cdedc067e
league <league@imac-14.attlocal.net>
2021-08-19 16:56:40 -0700

    GAME MAZE STARTED map 1 = done!

---
+++ src/_03_print_and_popups/_1_greeter/Greeter.java
@@ -0,0 +1,14 @@
+package _03_print_and_popups._1_greeter;
+
+import javax.swing.JOptionPane;
+
+public class Greeter {
+public static void main(String[] args) {
+String name	=JOptionPane.showInputDialog("Hello this is a Bot. What's Your name?");
+JOptionPane.showMessageDialog(null, "Hello, " + name);
+JOptionPane.showMessageDialog(null, "I Hope You don't mind, " + name + ", but I'll be looking around in your Computer >:D");
+JOptionPane.showMessageDialog(null, "Now, " + name + ", this is my computer now >:D");
+JOptionPane.showMessageDialog(null, "Just Joking, Bye!, " + name + " ;D");
+}
+}
+//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
